5715d2cc32be31be2b2ca17194be4f900ca126a2
yashvant999-design <yashvantcomputer555@gmail.com>
2026-03-20 15:19:25 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -17,17 +17,17 @@ const About = () => {
             <div id='aboutrow' style={{justifyContent:'center'}} className='row'>
                 <div id='aboutmain' className='col-lg-5'>
                     <div className="col-lg-6">
-                    <img id='aboutimg1' style={{Width:'500px'}}  src={a1}/>
+                    <img id='aboutimg1' style={{Width:'500px'}} src={a1} alt="about1"/>
                     </div>
                     <div className="col-lg-6">
-                    <img  id='aboutimg2' style={{marginTop:'20px',Width:'500px'}} src={a2}/>
+                    <img id='aboutimg2' style={{marginTop:'20px',Width:'500px'}} src={a2} alt="about2"/>
                     </div>
                 </div>
 
                 <div className='col-lg-7'>
                     <div className='hero-image'>
-                        <img  id='aboutimg3' style={{height:'510px',width:'100%'}}  src={a3}/>
-                        <div id='abouttext' style={{position:'relative'}} className='hero-text'>
+                            <img id='aboutimg3' style={{height:'510px',width:'100%'}} src={a3} alt="about3"/>                       
+                            <div id='abouttext' style={{position:'relative'}} className='hero-text'>
                             <h3 style={{color:'white',textAlign:'center',marginLeft:'-100px',marginTop:'30px'}}>Who We Are</h3>
                             <p style={{color:'white',textAlign:'center',width:'100%',marginLeft:'10px',justifyContent:'center'}}>Lorem ipsum dolor sit amet, consectetur<br/> adipiscing elit. Nunc vulputate libero et velit<br/> interdum, ac aliquet odio mattis. Class aptent<br/> taciti sociosqu ad litora torquent per conubia<br/> nostra, per inceptos himenaeos.Class aptent<br/> taciti sociosqu ad litora torquent per conubia <br/>nostra, per inceptos himenaeos.</p>
                             <button style={{marginLeft:'100px'}} className='btnabout'>Read More</button>
